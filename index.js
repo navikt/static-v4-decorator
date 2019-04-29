4d9ebb23a4aa0a8a751b6d0df3c313554e60c9fd
Morten Jansrud <morten@founder.no>
2019-04-29 12:31:39 +0200

    Legg til statiske filer fra prod

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.static("static"));
+
 const isProduction = process.env.NODE_ENV === "production";
 const allowedOrigin = isProduction
   ? `(http|https)://(.*).nav.no`
---
+++ static/_public/beta.nav.no/built-navno/js/navno/shinnloggingslinjen.js
@@ -0,0 +1,1 @@
+Innloggingslinje.init();
