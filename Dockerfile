339617d627abe146517e34fbce5e3e725af8579b
Morten Jansrud <morten@founder.no>
2019-04-29 10:22:33 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:11.7.0
+
+RUN ln -fs /usr/share/zoneinfo/Europe/Oslo /etc/localtime
+ENV NODE_ENV production
+ENV CI=true
+
+WORKDIR /app
+
+COPY index.js ./
+COPY static/ ./static
+COPY package.json ./
+COPY package-lock.json ./
+
+RUN npm install
+CMD ["node", "index.js"]
+
+EXPOSE 8080
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const isProduction = process.env.NODE_ENV === "production";
+const allowedOrigin = isProduction
+  ? `(http|https)://(.*).nav.no`
+  : `http://localhost:3000`;
+
+// Express settings
+app.use((req, res, next) => {
+  const origin = req.get("origin");
+  if (origin && origin.match(allowedOrigin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  }
+  next();
+});
+
+app.get("/isAlive", (req, res) => res.sendStatus(200));
+app.get("/isReady", (req, res) => res.sendStatus(200));
+app.get("/", (req, res) => res.sendStatus(200));
+app.listen(8080, () => console.log(`App listening on port: 8080`));
